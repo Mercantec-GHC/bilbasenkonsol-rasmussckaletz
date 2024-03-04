f426966b6f5c99fd0dcdbf01a598fceb5709d383
rasmussckaletz <rasmus@leeloo.dk>
2024-03-04 11:50:12 +0100

    added cars to Car array

--- Program.cs
+++ Program.cs
@@ -6,114 +6,115 @@ namespace BilbasenKonsol
     {
         static void Main(string[] args)
         {
-            //Eksempel med rigtige biler, lavet af ChatGPT ud fra jeres standart klasse
-            Car fordMustang1963 = new Car("Ford", "Mustang", 1963, "Red", 430);
-            Car toyotaCamry2020 = new Car("Toyota", "Camry", 2020, "Blue", 200);
-            Car chevyCorvette2022 = new Car("Chevrolet", "Corvette", 2022, "Silver", 650);
-            Car hondaCivic2005 = new Car("Honda", "Civic", 2005, "Black", 150);
-            Car bmw3Series2018 = new Car("BMW", "3 Series", 2018, "White", 320);
-            Car volkswagenGolf2019 = new Car("Volkswagen", "Golf", 2019, "Gray", 170);
-            Car audiA42021 = new Car("Audi", "A4", 2021, "Black", 248);
-            Car mercedesBenzEClass2017 = new Car("Mercedes-Benz", "E-Class", 2017, "Silver", 320);
-            Car teslaModelS2023 = new Car("Tesla", "Model S", 2023, "Blue", 750);
-            Car subaruOutback2015 = new Car("Subaru", "Outback", 2015, "Green", 175);
-            Car lexusRX3502019 = new Car("Lexus", "RX 350", 2019, "White", 295);
-            Car porsche9112021 = new Car("Porsche", "911", 2021, "Red", 443);
-            Car mazdaMX52016 = new Car("Mazda", "MX-5", 2016, "Yellow", 181);
-            Car jeepWrangler2018 = new Car("Jeep", "Wrangler", 2018, "Black", 285);
-            Car hyundaiElantra2017 = new Car("Hyundai", "Elantra", 2017, "Silver", 147);
-            Car kiaSorento2020 = new Car("Kia", "Sorento", 2020, "Blue", 185);
-            Car fordEscape2019 = new Car("Ford", "Escape", 2019, "Gray", 168);
-            Car toyotaRav42021 = new Car("Toyota", "RAV4", 2021, "White", 203);
-            Car chevySilverado2015 = new Car("Chevrolet", "Silverado", 2015, "Black", 355);
-            Car hondaAccord2016 = new Car("Honda", "Accord", 2016, "Red", 185);
-            Car bmw5Series2020 = new Car("BMW", "5 Series", 2020, "Silver", 335);
-            Car volvoXC902018 = new Car("Volvo", "XC90", 2018, "Blue", 316);
-            Car audiQ52019 = new Car("Audi", "Q5", 2019, "White", 248);
-            Car mercedesBenzGLC3002021 = new Car("Mercedes-Benz", "GLC 300", 2021, "Black", 255);
-            Car teslaModel32022 = new Car("Tesla", "Model 3", 2022, "Red", 318);
-            Car subaruForester2017 = new Car("Subaru", "Forester", 2017, "Green", 170);
-            Car lexusES3502020 = new Car("Lexus", "ES 350", 2020, "Silver", 302);
-            Car porscheCayenne2019 = new Car("Porsche", "Cayenne", 2019, "Blue", 340);
-            Car mazdaCX52017 = new Car("Mazda", "CX-5", 2017, "Gray", 187);
-            Car jeepGrandCherokee2016 = new Car("Jeep", "Grand Cherokee", 2016, "Black", 295);
-            Car hyundaiTucson2020 = new Car("Hyundai", "Tucson", 2020, "White", 161);
-            Car kiaSportage2018 = new Car("Kia", "Sportage", 2018, "Silver", 181);
-            Car fordExplorer2022 = new Car("Ford", "Explorer", 2022, "Red", 300);
-            Car toyotaTacoma2015 = new Car("Toyota", "Tacoma", 2015, "Black", 278);
-            Car chevyTahoe2019 = new Car("Chevrolet", "Tahoe", 2019, "White", 355);
-            Car hondaPilot2018 = new Car("Honda", "Pilot", 2018, "Blue", 280);
-            Car bmwX52020 = new Car("BMW", "X5", 2020, "Silver", 335);
-            Car volvoS602017 = new Car("Volvo", "S60", 2017, "Black", 316);
-            Car audiA62018 = new Car("Audi", "A6", 2018, "Gray", 335);
-            Car mercedesBenzCLA2502019 = new Car("Mercedes-Benz", "CLA 250", 2019, "White", 221);
-            Car teslaModelX2021 = new Car("Tesla", "Model X", 2021, "Red", 670);
-            Car subaruImpreza2016 = new Car("Subaru", "Impreza", 2016, "Blue", 152);
-            Car lexusIS3002018 = new Car("Lexus", "IS 300", 2018, "Black", 260);
-            Car porscheMacan2020 = new Car("Porsche", "Macan", 2020, "Silver", 348);
-            Car mazda32017 = new Car("Mazda", "3", 2017, "Red", 155);
-            Car jeepRenegade2019 = new Car("Jeep", "Renegade", 2019, "White", 180);
-            Car hyundaiSonata2022 = new Car("Hyundai", "Sonata", 2022, "Blue", 191);
-            Car kiaOptima2016 = new Car("Kia", "Optima", 2016, "Black", 185);
-            Car fordF1502018 = new Car("Ford", "F-150", 2018, "Silver", 395);
-            Car toyotaHighlander2017 = new Car("Toyota", "Highlander", 2017, "Gray", 295);
-            Car chevyEquinox2020 = new Car("Chevrolet", "Equinox", 2020, "White", 170);
-            Car hondaHRV2019 = new Car("Honda", "HR-V", 2019, "Red", 141);
-            Car bmw7Series2021 = new Car("BMW", "7 Series", 2021, "Black", 600);
-            Car volvoXC602019 = new Car("Volvo", "XC60", 2019, "Blue", 316);
-            Car audiQ72020 = new Car("Audi", "Q7", 2020, "Silver", 335);
-            Car mercedesBenzC3002016 = new Car("Mercedes-Benz", "C300", 2016, "White", 241);
-            Car teslaModelY2022 = new Car("Tesla", "Model Y", 2022, "Blue", 670);
-            Car subaruLegacy2015 = new Car("Subaru", "Legacy", 2015, "Black", 175);
-            Car lexusNX3002018 = new Car("Lexus", "NX 300", 2018, "Silver", 235);
-            Car porschePanamera2019 = new Car("Porsche", "Panamera", 2019, "Red", 400);
-            Car mazda62017 = new Car("Mazda", "6", 2017, "Gray", 187);
-            Car jeepCompass2018 = new Car("Jeep", "Compass", 2018, "White", 180);
-            Car hyundaiSantaFe2021 = new Car("Hyundai", "Santa Fe", 2021, "Blue", 235);
-            Car kiaForte2016 = new Car("Kia", "Forte", 2016, "Black", 147);
-            Car fordRanger2019 = new Car("Ford", "Ranger", 2019, "Silver", 270);
-            Car toyotaSienna2017 = new Car("Toyota", "Sienna", 2017, "Gray", 296);
-            Car chevyColorado2020 = new Car("Chevrolet", "Colorado", 2020, "White", 200);
-            Car hondaFit2018 = new Car("Honda", "Fit", 2018, "Red", 130);
-            Car bmwX32022 = new Car("BMW", "X3", 2022, "Black", 248);
-            Car volvoV902020 = new Car("Volvo", "V90", 2020, "Silver", 316);
-            Car audiA82019 = new Car("Audi", "A8", 2019, "White", 460);
-            Car mercedesBenzGLE4502021 = new Car("Mercedes-Benz", "GLE 450", 2021, "Blue", 362);
-            Car teslaCybertruck2023 = new Car("Tesla", "Cybertruck", 2023, "Silver", 800);
-            Car subaruCrosstrek2018 = new Car("Subaru", "Crosstrek", 2018, "Orange", 152);
-            Car lexusRX450h2020 = new Car("Lexus", "RX 450h", 2020, "Black", 308);
-            Car porsche718Cayman2020 = new Car("Porsche", "718 Cayman", 2020, "Yellow", 300);
-            Car mazdaCX92018 = new Car("Mazda", "CX-9", 2018, "White", 250);
-            Car jeepCherokee2017 = new Car("Jeep", "Cherokee", 2017, "Red", 271);
-            Car hyundaiKona2021 = new Car("Hyundai", "Kona", 2021, "Gray", 175);
-            Car kiaRio2016 = new Car("Kia", "Rio", 2016, "Blue", 130);
-            Car fordEdge2019 = new Car("Ford", "Edge", 2019, "Silver", 250);
-            Car toyotaCorolla2020 = new Car("Toyota", "Corolla", 2020, "White", 169);
-            Car chevyTraverse2018 = new Car("Chevrolet", "Traverse", 2018, "Black", 310);
-            Car hondaOdyssey2017 = new Car("Honda", "Odyssey", 2017, "Red", 280);
-            Car bmwX72021 = new Car("BMW", "X7", 2021, "Blue", 355);
-            Car volvoXC402020 = new Car("Volvo", "XC40", 2020, "Silver", 187);
-            Car audiQ32018 = new Car("Audi", "Q3", 2018, "Gray", 228);
-            Car mercedesBenzSClass2019 = new Car("Mercedes-Benz", "S-Class", 2019, "White", 463);
-            Car teslaRoadster2024 = new Car("Tesla", "Roadster", 2024, "Red", 1000);
-            Car subaruAscent2019 = new Car("Subaru", "Ascent", 2019, "Blue", 260);
-            Car lexusLC5002021 = new Car("Lexus", "LC 500", 2021, "Black", 471);
-            Car porsche911TurboS2023 = new Car("Porsche", "911 Turbo S", 2023, "Silver", 640);
-            Car mazdaCX32016 = new Car("Mazda", "CX-3", 2016, "White", 146);
-            Car jeepGladiator2020 = new Car("Jeep", "Gladiator", 2020, "Red", 285);
-            Car hyundaiVeloster2017 = new Car("Hyundai", "Veloster", 2017, "Yellow", 147);
-            Car kiaSoul2019 = new Car("Kia", "Soul", 2019, "Gray", 147);
-            Car fordExpedition2018 = new Car("Ford", "Expedition", 2018, "White", 375);
-            Car toyotaAvalon2021 = new Car("Toyota", "Avalon", 2021, "Black", 301);
-            Car chevyBlazer2019 = new Car("Chevrolet", "Blazer", 2019, "Blue", 305);
-            Car hondaRidgeline2020 = new Car("Honda", "Ridgeline", 2020, "Gray", 280);
+
+            Car[] cars = {
+                new Car("Ford", "Mustang", 1963, "Red", 430),
+                new Car("Toyota", "Camry", 2020, "Blue", 200),
+                new Car("Chevrolet", "Corvette", 2022, "Silver", 650),
+                new Car("Honda", "Civic", 2005, "Black", 150),
+                new Car("BMW", "3 Series", 2018, "White", 320),
+                new Car("Volkswagen", "Golf", 2019, "Gray", 170),
+                new Car("Audi", "A4", 2021, "Black", 248),
+                new Car("Mercedes-Benz", "E-Class", 2017, "Silver", 320),
+                new Car("Tesla", "Model S", 2023, "Blue", 750),
+                new Car("Subaru", "Outback", 2015, "Green", 175),
+                new Car("Lexus", "RX 350", 2019, "White", 295),
+                new Car("Porsche", "911", 2021, "Red", 443),
+                new Car("Mazda", "MX-5", 2016, "Yellow", 181),
+                new Car("Jeep", "Wrangler", 2018, "Black", 285),
+                new Car("Hyundai", "Elantra", 2017, "Silver", 147),
+                new Car("Kia", "Sorento", 2020, "Blue", 185),
+                new Car("Ford", "Escape", 2019, "Gray", 168),
+                new Car("Toyota", "RAV4", 2021, "White", 203),
+                new Car("Chevrolet", "Silverado", 2015, "Black", 355),
+                new Car("Honda", "Accord", 2016, "Red", 185),
+                new Car("BMW", "5 Series", 2020, "Silver", 335),
+                new Car("Volvo", "XC90", 2018, "Blue", 316),
+                new Car("Audi", "Q5", 2019, "White", 248),
+                new Car("Mercedes-Benz", "GLC 300", 2021, "Black", 255),
+                new Car("Tesla", "Model 3", 2022, "Red", 318),
+                new Car("Subaru", "Forester", 2017, "Green", 170),
+                new Car("Lexus", "ES 350", 2020, "Silver", 302),
+                new Car("Porsche", "Cayenne", 2019, "Blue", 340),
+                new Car("Mazda", "CX-5", 2017, "Gray", 187),
+                new Car("Jeep", "Grand Cherokee", 2016, "Black", 295),
+                new Car("Hyundai", "Tucson", 2020, "White", 161),
+                new Car("Kia", "Sportage", 2018, "Silver", 181),
+                new Car("Ford", "Explorer", 2022, "Red", 300),
+                new Car("Toyota", "Tacoma", 2015, "Black", 278),
+                new Car("Chevrolet", "Tahoe", 2019, "White", 355),
+                new Car("Honda", "Pilot", 2018, "Blue", 280),
+                new Car("BMW", "X5", 2020, "Silver", 335),
+                new Car("Volvo", "S60", 2017, "Black", 316),
+                new Car("Audi", "A6", 2018, "Gray", 335),
+                new Car("Mercedes-Benz", "CLA 250", 2019, "White", 221),
+                new Car("Tesla", "Model X", 2021, "Blue", 670),
+                new Car("Subaru", "Impreza", 2016, "Blue", 152),
+                new Car("Lexus", "IS 300", 2018, "Black", 260),
+                new Car("Porsche", "Macan", 2020, "Silver", 348),
+                new Car("Mazda", "3", 2017, "Red", 155),
+                new Car("Jeep", "Renegade", 2019, "White", 180),
+                new Car("Hyundai", "Sonata", 2022, "Blue", 191),
+                new Car("Kia", "Optima", 2016, "Black", 185),
+                new Car("Ford", "F-150", 2018, "Silver", 395),
+                new Car("Toyota", "Highlander", 2017, "Gray", 295),
+                new Car("Chevrolet", "Equinox", 2020, "White", 170),
+                new Car("Honda", "HR-V", 2019, "Red", 141),
+                new Car("BMW", "7 Series", 2021, "Black", 600),
+                new Car("Volvo", "XC60", 2019, "Blue", 316),
+                new Car("Audi", "Q7", 2020, "Silver", 335),
+                new Car("Mercedes-Benz", "C300", 2016, "White", 241),
+                new Car("Tesla", "Model Y", 2022, "Blue", 670),
+                new Car("Subaru", "Legacy", 2015, "Black", 175),
+                new Car("Lexus", "NX 300", 2018, "Silver", 235),
+                new Car("Porsche", "Panamera", 2019, "Red", 400),
+                new Car("Mazda", "6", 2017, "Gray", 187),
+                new Car("Jeep", "Compass", 2018, "White", 180),
+                new Car("Hyundai", "Santa Fe", 2021, "Blue", 235),
+                new Car("Kia", "Forte", 2016, "Black", 147),
+                new Car("Ford", "Ranger", 2019, "Silver", 270),
+                new Car("Toyota", "Sienna", 2017, "Gray", 296),
+                new Car("Chevrolet", "Colorado", 2020, "White", 200),
+                new Car("Honda", "Fit", 2018, "Red", 130),
+                new Car("BMW", "X3", 2022, "Black", 248),
+                new Car("Volvo", "V90", 2020, "Silver", 316),
+                new Car("Audi", "A8", 2019, "White", 460),
+                new Car("Mercedes-Benz", "GLE 450", 2021, "Blue", 362),
+                new Car("Tesla", "Cybertruck", 2023, "Silver", 800),
+                new Car("Subaru", "Crosstrek", 2018, "Orange", 152),
+                new Car("Lexus", "RX 450h", 2020, "Black", 308),
+                new Car("Porsche", "718 Cayman", 2020, "Yellow", 300),
+                new Car("Mazda", "CX-9", 2018, "White", 250),
+                new Car("Jeep", "Cherokee", 2017, "Red", 271),
+                new Car("Hyundai", "Kona", 2021, "Gray", 175),
+                new Car("Kia", "Rio", 2016, "Blue", 130),
+                new Car("Ford", "Edge", 2019, "Silver", 250),
+                new Car("Toyota", "Corolla", 2020, "White", 169),
+                new Car("Chevrolet", "Traverse", 2018, "Black", 310),
+                new Car("Honda", "Odyssey", 2017, "Red", 280),
+                new Car("BMW", "X7", 2021, "Blue", 355),
+                new Car("Volvo", "XC40", 2020, "Silver", 187),
+                new Car("Audi", "Q3", 2018, "Gray", 228),
+                new Car("Mercedes-Benz", "S-Class", 2019, "White", 463),
+                new Car("Tesla", "Roadster", 2024, "Red", 1000),
+                new Car("Subaru", "Ascent", 2019, "Blue", 260),
+                new Car("Lexus", "LC 500", 2021, "Black", 471),
+                new Car("Porsche", "911 Turbo S", 2023, "Silver", 640),
+                new Car("Mazda", "CX-3", 2016, "White", 146),
+                new Car("Jeep", "Gladiator", 2020, "Red", 285),
+                new Car("Hyundai", "Veloster", 2017, "Yellow", 147),
+                new Car("Kia", "Soul", 2019, "Gray", 147),
+                new Car("Ford", "Expedition", 2018, "White", 375),
+                new Car("Toyota", "Avalon", 2021, "Black", 301),
+                new Car("Chevrolet", "Blazer", 2019, "Blue", 305),
+                new Car("Honda", "Ridgeline", 2020, "Gray", 280)
+            };
 
 
-            Console.WriteLine(fordMustang1963.Brand);
-            Console.WriteLine(toyotaCamry2020.Brand);
-            Console.WriteLine(chevyCorvette2022.Brand);
-            //Det er vigtigt at I finder en god datatype til at opbevare jeres biler / Objekter!
-
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.Model);
+            }
         }
     }
 }
